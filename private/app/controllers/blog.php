61d270a1f4573dfa547969969e3f8601a540e5ed
likhithlikki08 <likhithlikki08@gmail.com>
2020-04-23 01:09:41 +0000

    final project 1

---
+++ private/app/controllers/blog.php
@@ -0,0 +1,45 @@
+<?php
+class Blog extends Controller
+
+
+
+{
+    function _construct(){
+        parent::__construct();
+    }
+    function Index(){
+        $this -> model("BlogModel");
+        $posts = $this -> BlogModel -> getAllPosts();
+        $input = Array("posts" => $posts);
+        $this -> view("template/header");
+        $this -> view("blog/index" , $input);
+        $this -> view("template/footer");
+    }
+    function Read($postId){
+        $this -> model("BlogModel");
+        $post = $this -> BlogModel -> getPostById($postId);
+        $this -> view ("blog/header" , $post);
+        $this -> view("template/footer");
+    }
+    function Create(){
+        $is_auth = isset($_SESSION["username"]);
+        if(!is_auth){
+            header("loaction: /blog");
+            
+            return;
+        }
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $title = $_POST["title"];
+            $content = $_POST["content"];
+            $author = $_POST["author"];
+            $this -> model("BlogModel");
+            $slug = $this -> BlogModel -> createPost($title , $author , $content);
+            header("location: /blog/read/" .$slug);    
+        }else{
+            $this -> view("template/header");
+            $this -> view("blog/create");
+            $this -> view("template/footer");
+        }
+    }
+}
+?>
