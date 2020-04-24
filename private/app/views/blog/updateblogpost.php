e04c8e4c0c8902064b1728222335fb7ebbfa37c8
likhithlikki08 <likhithlikki08@gmail.com>
2020-04-24 06:07:59 +0000

    pj final 33

---
+++ private/app/views/blog/updateblogpost.php
@@ -0,0 +1,13 @@
+<h1>Updating Blog Post</h1>
+
+<form method = "post" action = "/blog/update/<?php echo $slug?>">
+<input type = "hidden" value = "<?php echo $slug?>" name = "slug"/>
+<label for = "title">Title</label>
+<input type = "text" id = "title" name = "title" value = "<?php echo $title?>"/>
+<label for = "author">Author</label>
+<input type = "email" id = "author" name = "author" value = "<?php echo $author?>"/>
+<label for = "content">Content</label>
+<textarea name = "content" id = "content"><?php echo $content?></textarea>
+<input type = "submit" value = "submit">
+
+</form>
